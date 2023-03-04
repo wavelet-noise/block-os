a5809a9234b8f21f18da7e143b6b0a0f3fce6669
wavelet-noise <ishellstrike@gmail.com>
2023-03-04 12:16:25 +0300

    Create bios.lua

---
+++ bios.lua
@@ -0,0 +1,26 @@
+local fucntion init(self, cache)
+  local function boot_invoke(file)
+    local result = table.pack(pcall(self.run_file, file, ...))
+    if not result[1] then
+      return nil, result[2]
+    else
+      return table.unpack(result, 2, result.n)
+    end
+  end
+
+  local function load_from_default()
+    local handle, reason = boot_invoke("init.lua")
+    if not handle then
+      return nil, reason
+    end
+  end
+  
+  local reason
+  init, reason = load_from_default()
+  if not init then
+    error("no bootable medium found" .. (reason and (": " .. tostring(reason)) or ""), 0)
+  end
+  
+  self.beep(1000, 0.2)
+end
+return init()
